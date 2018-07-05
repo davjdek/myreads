1319c28080778e9a1ee3d1a16e83b8b2e6bfbb88
davjdek <davide.sacquegna@libero.it>
2018-07-05 09:46:46 +0200

    Added project required features

---
+++ src/book.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const Book = (props) => {
+  const { book: { imageLinks, title, authors, shelf }, handleShelfUpdate } = props;
+  const thumbnail = imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : null;
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ backgroundImage: `url('${thumbnail}')`}}></div>
+			  {console.log(thumbnail)}
+          <div className="book-shelf-changer">
+            <select value={shelf || 'none'} onChange={(event => handleShelfUpdate(props.book, event.target.value))}>
+              <option value="na" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{!!authors && authors.join(', ')}</div>
+      </div>
+    </li>
+  )
+}
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  handleShelfUpdate: PropTypes.func.isRequired
+}
+
+export default Book;
---
+++ src/search-page.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
+import { debounce } from 'throttle-debounce';
+
+import * as BooksAPI from './books'
+import BookList from './book-list';
+
+const maxResults = 20;
+
+class SearchPage extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    handleShelfUpdate: PropTypes.func.isRequired
+  }
+
+  state = {
+    results: []
+  }
+
+  updateResults = (updatedBook, shelf) => {
+    const index = this.state.results.indexOf(updatedBook);
+    const newBook = Object.assign({}, updatedBook, { shelf: shelf });
+    this.setState({
+      results: [
+        ...this.state.results.slice(0, index),
+        newBook,
+        ...this.state.results.slice(index + 1),
+      ]
+    });
+  }
+
+  onChange = (event) => {
+    this.searchBooks(event.target.value.trim());
+  }
+
+  searchBooks = debounce(1000, (query) => {
+    BooksAPI.search(query, maxResults)
+      .then(response => {
+        this.props.books.forEach(shelvedBook => {
+			console.log(response);
+          const match = response.find((resultBook) => resultBook.id === shelvedBook.id);
+          if (match) {
+            match.shelf = shelvedBook.shelf;  
+          }
+        })
+        this.setState({ results: response });
+    });
+  })
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className='search-books-bar'>
+          <Link className="close-search" to='/'/>
+          <div className='search-books-input-wrapper'>
+            <input 
+              type='text'
+              placeholder='Search by title or author'
+              onChange={this.onChange}
+            />
+          </div>
+        </div>
+        { this.state.results.length !== 0 && (
+          <div className="search-books-results">
+            <BookList 
+              list={this.state.results}
+              handleShelfUpdate={(book, value) => {
+                this.props.handleShelfUpdate(book, value);
+                this.updateResults(book, value);
+              }}
+            />
+          </div> 
+        )}
+      </div>
+    )
+  }
+}
+
+export default SearchPage;
